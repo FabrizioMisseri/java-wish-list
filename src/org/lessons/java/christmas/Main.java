97efcc1fdff5a6a73d7afabf48d38d58be79b764
FabrizioMisseri <misserifabrizio@gmail.com>
2023-03-16 14:11:39 +0100

    exe1 completed

---
+++ src/org/lessons/java/christmas/Main.java
@@ -0,0 +1,26 @@
+package org.lessons.java.christmas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        List<String> whishes = new ArrayList<>();
+        boolean flagList = true;
+        Scanner scan = new Scanner(System.in);
+
+        while(flagList){
+            System.out.println("cosa desideri per natale?");
+            String wish = scan.nextLine();
+            whishes.add(wish);
+            System.out.println("hai " + whishes.size() + " numero di richieste");
+            System.out.println("decidi aggiungerne un altra? s/n");
+            if(scan.nextLine().equalsIgnoreCase("n")) {
+                flagList = !flagList;
+            }
+        }
+
+        System.out.println("hai espresso i seguenti desideri: " + "\n" + whishes);
+    }
+}
